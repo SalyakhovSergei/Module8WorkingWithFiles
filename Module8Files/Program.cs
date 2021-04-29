ed1097deffcd2be084c6ef8a91e7a47d5be697ec
Sergey <ssm1988@inbox.ru>
2021-04-29 17:25:50 +1000

    Добавьте файлы проекта.

---
+++ Module8Files/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Module8Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DeleteFiles("C:/Users/Анатолий/Desktop/testFolder");
+
+            static void DeleteFiles(string folder)
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(folder);
+                DirectoryInfo[] directories = dirInfo.GetDirectories();
+                FileInfo[] FI = dirInfo.GetFiles();
+
+                if (dirInfo.Exists)
+                {
+                    foreach (FileInfo file in FI)
+                    {
+                        file.Delete();
+                    }
+                    foreach (DirectoryInfo DI in directories)
+                    {
+                        DeleteFiles(DI.FullName);
+                    }
+                }
+            }
+        }
+    }
+}
