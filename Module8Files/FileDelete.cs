f714d4cd91ebb1b28f67e39ea7119f9d15a622c6
Саляхов Сергей <salyakhovsergei@gmail.com>
2021-04-29 20:56:42 +1000

    task1

---
+++ Module8Files/FileDelete.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module8Files
+{
+    public class FileDelete
+    {
+        public void DeleteFiles(string folder)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(folder);
+            DirectoryInfo[] directories = dirInfo.GetDirectories();
+            FileInfo[] FI = dirInfo.GetFiles();
+
+            if (dirInfo.Exists)
+            {
+                foreach (FileInfo file in FI)
+                {
+                    TimeSpan fileTime = DateTime.Now - file.CreationTime;
+                    if (fileTime.Minutes > 30)
+                    {
+                        file.Delete();
+                    }
+                }
+                foreach (DirectoryInfo DI in directories)
+                {
+                    DeleteFiles(DI.FullName);
+                }
+            }
+        }
+    }
+}
--- Module8Files/Program.cs
+++ Module8Files/Program.cs
@@ -7,30 +7,11 @@ namespace Module8Files
     {
         static void Main(string[] args)
         {
-            DeleteFiles("C:/Users/admin/Desktop/1212");
+            FileDelete test = new FileDelete();
 
-            static void DeleteFiles(string folder)
-            {
-                DirectoryInfo dirInfo = new DirectoryInfo(folder);
-                DirectoryInfo[] directories = dirInfo.GetDirectories();
-                FileInfo[] FI = dirInfo.GetFiles();
+            test.DeleteFiles("C:/Users/admin/Desktop/1212");
 
-                if (dirInfo.Exists)
-                {
-                    foreach (FileInfo file in FI)
-                    {
-                        TimeSpan fileTime = DateTime.Now - file.CreationTime;
-                        if (fileTime.Minutes > 30)
-                        {
-                            file.Delete();
-                        }
-                    }
-                    foreach (DirectoryInfo DI in directories)
-                    {
-                        DeleteFiles(DI.FullName);
-                    }
-                }
-            }
+            
         }
     }
 }
